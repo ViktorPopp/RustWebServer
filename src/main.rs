63332a30b10c90f4326fccd5685d47bd5c33d6a5
Viktor Popp <viktorpopp.dev@gmail.com>
2025-02-05 12:08:37 +0100

    Used `match` statement instead of `if`

--- src/main.rs
+++ src/main.rs
@@ -18,18 +18,15 @@ fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&stream);
     let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
-        ("HTTP/1.1 200 OK", "www/index.html")
-    } else {
-        ("HTTP/1.1 404 NOT FOUND", "www/404.html")
+    let (status_line, filename) = match &request_line[..] {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "www/index.html"),
+        _ => ("HTTP/1.1 404 NOT FOUND", "www/404.html"),
     };
 
     let contents = fs::read_to_string(filename).unwrap();
     let length = contents.len();
 
-    let response =
-        format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
 
     stream.write_all(response.as_bytes()).unwrap();
-
 }
